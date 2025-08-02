43699cea7cd9307df4a8628fddb6e25dbd431cff
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-02 10:42:39 +0200

    add sprite

--- semaine-03-poo-api/mini-projet/main.py
+++ semaine-03-poo-api/mini-projet/main.py
@@ -1,13 +1,15 @@
 import requests
+from IPython.display import Image, display
 
 class Pokemon:
-  def __init__(self, name, type1, type2, hp, attack, defense):
+  def __init__(self, name, type1, type2, hp, attack, defense, sprite):
     self.name = name
     self.type1 = type1
     self.type2 = type2
     self.hp = hp
     self.attack = attack
     self.defense = defense
+    self.sprite = sprite
 
   def __str__(self):
     description = f"{self.name} is a Pokémon of type {self.type1}"
@@ -15,6 +17,9 @@ class Pokemon:
       description += f" and {self.type2}"
     description += f"\nHP: {self.hp} - Attack: {self.attack} - Defense: {self.defense}"
     return description
+
+  def show_pokemon_sprite(self):
+    display(Image(url=self.sprite))
 
 def search_pokemon():
   asked_pokemon = input('Enter a Pokemon name: ')
@@ -39,15 +44,21 @@ def search_pokemon():
     attack = stat_map.get('attack', 0)
     defense = stat_map.get('defense', 0)
 
+    sprites = decoded_response['sprites']
+    sprite = sprites['front_default']
+
     pokemon = Pokemon(
       decoded_response['name'].capitalize(),
       type1,
       type2,
       hp,
       attack,
-      defense
+      defense,
+      sprite
     )
 
+    pokemon.show_pokemon_sprite()
+    
     return pokemon
 
 def main():
