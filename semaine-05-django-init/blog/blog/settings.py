95556a8068da6e927088eedb186b584fc66f8bfa
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-11 10:27:05 +0200

    export key in .env file

--- semaine-05-django-init/blog/blog/settings.py
+++ semaine-05-django-init/blog/blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t397gpcd2^t#=_wm4!5sm!)_#yynvk3ecy-cu=b8r$*h(p8ka_'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY environment variable not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
