d43446ee88bb4d5f3ff78cc10cea6273d78c8feb
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-12 17:09:23 +0200

    first try to create class

--- semaine-05-django-init/blog/blog/settings.py
+++ semaine-05-django-init/blog/blog/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts'
 ]
 
 MIDDLEWARE = [
