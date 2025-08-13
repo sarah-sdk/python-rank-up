e1e9c7060e644eba204c62bcd180129ab3d487ea
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-13 10:18:30 +0200

    create posts view and urls

--- semaine-05-django-init/blog/posts/views.py
+++ semaine-05-django-init/blog/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
+def posts_list(request):
+  posts = Post.objects.all()
+  print(posts)
+  return HttpResponse(', '.join([p.title for p in posts]))
+  # return render(request, 'posts/list.html', {'posts': posts})
