08b6297e1e7dc057831e16a6b8fd342fbfea0185
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-13 11:44:12 +0200

    create template for posts

--- semaine-05-django-init/blog/posts/views.py
+++ semaine-05-django-init/blog/posts/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
 def posts_list(request):
   posts = Post.objects.all()
   print(posts)
-  return HttpResponse(', '.join([p.title for p in posts]))
-  # return render(request, 'posts/list.html', {'posts': posts})
+  return render(request, 'posts/list.html', {'posts': posts})
