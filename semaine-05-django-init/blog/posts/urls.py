0bcb2fcf8fcdce0d57d9a5a22df4b972bbff2fcb
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-14 20:39:20 +0200

    edit and delete post

--- semaine-05-django-init/blog/posts/urls.py
+++ semaine-05-django-init/blog/posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.posts_list, name='posts_list'),
   path('add/', views.post_create, name='post_create'),
+  path('<pk>/edit/', views.post_edit, name='post_edit'),
+  path('<pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- semaine-05-django-init/blog/posts/views.py
+++ semaine-05-django-init/blog/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post
 from .forms import PostForm
 
@@ -13,9 +13,29 @@ def post_create(request):
     form = PostForm(request.POST)
     if form.is_valid():
       form.save()
-      return redirect('/posts/')
+      return redirect('posts_list')
     
   else:
     form = PostForm()
   
   return render(request, 'posts/form.html', {'form': form})
+
+def post_edit(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  if request.method == 'POST':
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+      form.save()
+      return redirect('posts_list')
+    
+  else:
+    form = PostForm(instance=post)
+    return render(request, 'posts/form.html', {'form': form})
+    
+def post_delete(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  if request.method == 'POST':
+    post.delete()
+    return redirect('posts_list')
+  return render(request, 'posts/confirm_delete.html', {'post': post})
+    
