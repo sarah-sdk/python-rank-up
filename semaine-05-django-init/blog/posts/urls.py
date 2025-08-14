55c6ac471f7eb28e9a00b3ce21a33293b039aa1c
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-14 19:52:19 +0200

    create post creation

--- semaine-05-django-init/blog/posts/urls.py
+++ semaine-05-django-init/blog/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('posts/', views.posts_list, name='posts_list'),
+  path('', views.posts_list, name='posts_list'),
+  path('add/', views.post_create, name='post_create'),
 ]
--- semaine-05-django-init/blog/posts/views.py
+++ semaine-05-django-init/blog/posts/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def posts_list(request):
   posts = Post.objects.all()
   print(posts)
   return render(request, 'posts/list.html', {'posts': posts})
+
+def post_create(request):
+  if request.method == 'POST':
+    form = PostForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('/posts/')
+    
+  else:
+    form = PostForm()
+  
+  return render(request, 'posts/form.html', {'form': form})
