c633899289501996eb43301c93f0eb74ddcd99fa
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-22 12:26:42 +0200

    add task in admin interface and filter task by user

--- semaine-06-tests-sql/tasks/views.py
+++ semaine-06-tests-sql/tasks/views.py
@@ -5,3 +5,9 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
   queryset = Task.objects.all()
   serializer_class = TaskSerializer
+
+  def get_queryset(self):
+    return Task.objects.filter(user=self.request.user)
+  
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
