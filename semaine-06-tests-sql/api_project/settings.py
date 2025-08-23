0b6e2e7dc1d588c93ee164857a84680bd936dc1b
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-23 09:18:23 +0200

    add filtering and ordering fields

--- semaine-06-tests-sql/api_project/settings.py
+++ semaine-06-tests-sql/api_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'exercices',
     'pokemon',
-    'tasks'
+    'tasks',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
--- semaine-06-tests-sql/tasks/views.py
+++ semaine-06-tests-sql/tasks/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
   queryset = Task.objects.all()
   serializer_class = TaskSerializer
+  filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+  filterset_fields = ['done']
+  ordering_fields = ['created_at', 'title']
+  ordering = ['created_at']
 
   def get_queryset(self):
     return Task.objects.filter(user=self.request.user)
