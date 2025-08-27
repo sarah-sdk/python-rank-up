0df0c8f8569da413210320a14000430bdba5324f
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-27 13:42:20 +0200

    patch done status in front and backend !

--- semaine-07-projet/frontend/src/components/Item/Item.tsx
+++ semaine-07-projet/frontend/src/components/Item/Item.tsx
@@ -2,12 +2,31 @@ import { useState } from "react";
 import { Checked, Unchecked } from "../../../public/Checkbox";
 import type { ItemType } from "../../types/types";
 
+const token = import.meta.env.VITE_USER_TOKEN;
+const url = import.meta.env.VITE_API_URL;
+
 export default function Item({ item }: { item: ItemType }) {
   const [isChecked, setIsChecked] = useState<boolean>(item.done);
 
-  const handleChecked = () => {
-    setIsChecked(!isChecked);
+  const handleChecked = async () => {
+    const updateDone = {
+      done: !item.done,
+    };
+
+    const response = await fetch(`${url}/items/${item.id}/`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify(updateDone),
+    });
+
+    const data = await response.json();
+
+    setIsChecked(data.done);
   };
+
   return (
     <>
       <button type="button" onClick={handleChecked}>
