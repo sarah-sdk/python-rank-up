3909ebb4cf578469d81dc5fd0cd4190ecc50ed70
Sarah Smandack <sarah.smandack@gmail.com>
2025-08-28 15:19:32 +0200

    export modal in component

--- semaine-07-projet/frontend/src/App.tsx
+++ semaine-07-projet/frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import { useLoaderData, useRevalidator } from "react-router-dom";
 import { Add } from "../public/Add";
+import AddItemModal from "./components/AddItemModal/AddItemModal";
 import Item from "./components/Item/Item";
 import NavBar from "./components/NavBar/NavBar";
 import type { ItemType, UserType } from "./types/types";
 import "./App.css";
-import { useState } from "react";
 
 export default function App() {
   const token = import.meta.env.VITE_USER_TOKEN;
@@ -18,22 +19,17 @@ export default function App() {
   };
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [formData, setFormData] = useState<Omit<ItemType, "id">>({
-    name: "",
-    done: false,
-    category: "",
-  });
 
   const handleOpen = () => {
     setIsOpen(true);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (item: Omit<ItemType, "id">) => {
     const newItem = {
       user: user.id,
-      name: formData.name,
-      done: formData.done,
-      category: formData.category,
+      name: item.name,
+      done: item.done,
+      category: item.category,
     };
 
     const response = await fetch(`${url}/items/`, {
@@ -66,64 +62,13 @@ export default function App() {
           </figure>
         ))}
 
-        {isOpen && (
-          <div>
-            <h3>Ajouter un item à la liste</h3>
-            <form method="dialog" onSubmit={handleSubmit}>
-              <label htmlFor="nameItem">Nom de l'item : </label>
-              <input
-                type="text"
-                id="nameItem"
-                onChange={(e) => {
-                  setFormData((prev) => ({ ...prev, name: e.target.value }));
-                }}
-                required
-              />
-
-              <label htmlFor="select-category">Catégorie : </label>
-              <select
-                name="categories"
-                id="select-category"
-                onChange={(e) => {
-                  setFormData((prev) => ({
-                    ...prev,
-                    category: e.target.value,
-                  }));
-                }}
-              >
-                <option value="">--Choisis une catégorie--</option>
-                <option value="travel">Voyage</option>
-                <option value="reading">Lecture</option>
-                <option value="exp">Expérience</option>
-                <option value="sport">Sport</option>
-                <option value="skill">Compétence</option>
-                <option value="other">Autre</option>
-              </select>
-
-              <label htmlFor="doneItem">Réalisé : </label>
-              <input
-                type="checkbox"
-                id="doneItem"
-                onChange={(e) => {
-                  setFormData((prev) => ({
-                    ...prev,
-                    done: e.target.value === "on",
-                  }));
-                }}
-              />
-
-              <button type="submit">Ajouter</button>
-              <button
-                type="button"
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              >
-                Annuler
-              </button>
-            </form>
-          </div>
-        )}
+        <AddItemModal
+          isOpen={isOpen}
+          onClose={() => {
+            setIsOpen(false);
+          }}
+          onSubmit={handleSubmit}
+        />
       </main>
     </>
   );
---
+++ semaine-07-projet/frontend/src/components/AddItemModal/AddItemModal.tsx
@@ -0,0 +1,86 @@
+import { type FormEvent, useEffect, useRef, useState } from "react";
+import type { ItemType } from "../../types/types";
+
+export default function AddItemModal({
+  isOpen,
+  onClose,
+  onSubmit,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (item: Omit<ItemType, "id">) => void;
+}) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const [formData, setFormData] = useState<Omit<ItemType, "id">>({
+    name: "",
+    done: false,
+    category: "",
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+    }
+  }, [isOpen]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await onSubmit(formData);
+    dialogRef.current?.close();
+  };
+
+  return (
+    <dialog ref={dialogRef}>
+      <h3>Ajouter un item à la liste</h3>
+      <form method="dialog" onSubmit={handleSubmit}>
+        <label htmlFor="nameItem">Nom de l'item : </label>
+        <input
+          type="text"
+          id="nameItem"
+          onChange={(e) => {
+            setFormData((prev) => ({ ...prev, name: e.target.value }));
+          }}
+          required
+        />
+
+        <label htmlFor="select-category">Catégorie : </label>
+        <select
+          name="categories"
+          id="select-category"
+          onChange={(e) => {
+            setFormData((prev) => ({
+              ...prev,
+              category: e.target.value,
+            }));
+          }}
+        >
+          <option value="">--Choisis une catégorie--</option>
+          <option value="travel">Voyage</option>
+          <option value="reading">Lecture</option>
+          <option value="exp">Expérience</option>
+          <option value="sport">Sport</option>
+          <option value="skill">Compétence</option>
+          <option value="other">Autre</option>
+        </select>
+
+        <label htmlFor="doneItem">Réalisé : </label>
+        <input
+          type="checkbox"
+          id="doneItem"
+          onChange={(e) => {
+            setFormData((prev) => ({
+              ...prev,
+              done: e.target.checked,
+            }));
+          }}
+        />
+
+        <button type="submit">Ajouter</button>
+        <button type="button" onClick={onClose}>
+          Annuler
+        </button>
+      </form>
+    </dialog>
+  );
+}
